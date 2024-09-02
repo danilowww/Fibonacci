3685ebd40ae4904b53d7424cd317af324fa6de82
danilowww <71724566+danilowww@users.noreply.github.com>
2024-09-02 11:33:21 -0300

    Add files via upload

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que insira um número
+        Console.Write("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        if (int.TryParse(Console.ReadLine(), out int numero))
+        {
+            // Verifica se o número pertence à sequência de Fibonacci
+            if (PertenceFibonacci(numero))
+            {
+                Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Por favor, insira um número inteiro.");
+        }
+    }
+
+    static bool PertenceFibonacci(int n)
+    {
+        int a = 0, b = 1;
+
+        if (n < 0)
+        {
+            return false;
+        }
+
+        else if (n == a || n == b)
+        {
+            return true;
+        }
+
+        else
+        {
+            while (b < n)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                Console.WriteLine($"a = {a}, b = {b} e temp = {temp}");
+            }
+
+            return b == n;
+        }
+    }
+}
